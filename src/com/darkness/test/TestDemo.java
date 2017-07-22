52eb6588897c842f4c3f27e0c472c9c59f1324a2
nguyenvantra <dkdarknessnvt@gmail.com>
2017-07-22 16:53:24 +0700

    add life cycle JUnit annotation

--- src/com/darkness/test/TestDemo.java
+++ src/com/darkness/test/TestDemo.java
@@ -1,5 +1,39 @@
 package com.darkness.test;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class TestDemo {
+	@Before
+	public void before(){
+		System.out.println("Before");
+	}
 	
+	@After
+	public void after(){
+		System.out.println("After");
+	}
+	
+	@Test
+	public void test1(){
+		System.out.println("Test 1 run");
+	}
+	
+	@Test
+	public void test2(){
+		System.out.println("Test 2 run");
+	}
+	
+	@BeforeClass
+	public static void beforeClass(){
+		System.out.println("BeforeClass");
+	}
+	
+	@AfterClass
+	public static void afterClass(){
+		System.out.println("AfterClass");
+	}
 }
