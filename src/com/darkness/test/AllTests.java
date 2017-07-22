06610430cc5a4f42ace508b91e5b6012dfe954af
nguyenvantra <dkdarknessnvt@gmail.com>
2017-07-22 22:35:31 +0700

    add Test Suites

---
+++ src/com/darkness/test/AllTests.java
@@ -0,0 +1,11 @@
+package com.darkness.test;
+
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses({
+	FirstClassTest.class,
+	SecondClassTest.class
+})
+public class AllTests {
+	//do something test
+}
---
+++ src/com/darkness/test/SecondClassTest.java
@@ -0,0 +1,14 @@
+package com.darkness.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SecondClassTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
